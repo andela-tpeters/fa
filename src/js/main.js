4c43443b44429d8553048e479d1ef4570d995991
Tijesunimi Peters <tijesunimi.peters@andela.com>
2017-02-24 20:59:27 +0100

    editable profile

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,6 @@
+//turn to inline mode
+$.fn.editable.defaults.mode = 'inline';
+
 $("#more-menu-icon").click(function() {
 	$('.menu.extra').css('display', 'inline-block');
 });
@@ -20,40 +23,11 @@ $('#mobile-menu-profile-close').click(function() {
 
 $('#trial').collapse('toggle');
 
-// $(function() {
-// 	var xhr;
-// 	var select_state, $select_state;
-// 	var select_city, $select_city;
-
-// 	$select_state = $('#selector').selectize({
-// 	    onChange: function(value) {
-// 	        if (!value.length) return;
-// 	        select_city.disable();
-// 	        select_city.clearOptions();
-// 	        select_city.load(function(callback) {
-// 	            xhr && xhr.abort();
-// 	            xhr = $.ajax({
-// 	                url: 'https://jsonp.afeld.me/?url=http://api.sba.gov/geodata/primary_city_links_for_state_of/' + value + '.json',
-// 	                success: function(results) {
-// 	                    select_city.enable();
-// 	                    callback(results);
-// 	                },
-// 	                error: function() {
-// 	                    callback();
-// 	                }
-// 	            })
-// 	        });
-// 	    }
-// 	});
-
-// 	$select_city = $('#select-cities-city').selectize({
-// 	    valueField: 'name',
-// 	    labelField: 'name',
-// 	    searchField: ['name']
-// 	});
-
-// 	select_city  = $select_city[0].selectize;
-// 	select_state = $select_state[0].selectize;
-
-// 	select_city.disable();
-// });
+$(document).ready(function() {
+    $('#bio-description').editable({inputclass: 'twelve columns', rows: 50});
+    $('#profile-info-email').editable({inputclass: 'twelve columns'});
+    $('#profile-info-budiness-name').editable({inputclass: 'twelve columns'});
+    $('#profile-info-budiness-address').editable({inputclass: 'twelve columns'});
+    $('#profile-info-budiness-tax').editable({inputclass: 'twelve columns'});
+    $('#profile-name').editable({inputclass: 'twelve columns'});
+});
